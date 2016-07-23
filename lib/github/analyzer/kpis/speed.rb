67eef42e17d09f578025d1f4a962b09221bf51b1
mizhal <mizhal@gmail.com>
2016-07-23 11:11:12 +0000

    partial implementation of dashboard indicators

--- lib/github/analyzer/kpis/speed.rb
+++ lib/github/analyzer/kpis/speed.rb
@@ -39,13 +39,25 @@ module Github
               data = @catalog.GithubRawContributionCount.last_year
                 .order_by("date asc")
 
+              ## al these processing pipelines will be translated to mongo
+              ## eventually, if I would found convenient to delegate this
+              ## to data layer. But not now.
               kpis = {
                 speed_per_year: speed_per_year(data),
                 speed_per_month: speed_per_month(data),
                 speed_per_week: speed_per_week(data),
-                last_month_projection: last_month_projection(data),
-                last_week_projection: last_week_projection(data)
               }
+
+              kpis = kpis.merge(
+                current_week_speed: current_week_speed(kpis),
+                last_week_speed: last_week_speed(kpis),
+                current_month_speed: current_month_speed(kpis),
+                last_month_speed: last_month_speed(kpis),
+                current_week_projection: current_week_projection(kpis),
+                current_month_projection: current_month_projection(kpis),
+                last_month_projection: last_month_projection(kpis),
+                last_week_projection: last_week_projection(kpis)
+              )
 
               @custom_ranges.each do |range|
                 data = @catalog.GithubRawContributionCount.in_range(range.start, range.end)
@@ -63,7 +75,7 @@ module Github
 
             break if @stop
           end
-          
+
         end
 
         def stop
@@ -82,13 +94,71 @@ module Github
 
         def speed_per_month data
           ## data -(map)-> KEY: month -(reduce)-> SUM
+          ## I think this could be better done inside Mongo itself
           data.lazy.map{|entry| [get_month(entry.date), entry]}
             .group_by{|month, entry| month}
             .map{|month, entries| [month, entries.collect(&:count).sum]}
         end
 
+        def speed_per_week data
+          ## data -(map)-> KEY: week -(reduce)-> SUM
+          ## I think this could be better done inside Mongo itself
+          data.lazy.map{|entry| [get_week(entry.date), entry]}
+            .group_by{|week, entry| entry}
+            .map{|week, entries| [week, entries.collect(&:count).sum]}
+        end
+
+        def current_week_speed(kpis)
+          cweek = DateTime.now.cweek
+
+          kpis[:speed_per_week][cweek]
+        end
+
+        def last_week_speed(kpis)
+          lweek = DateTime.now.cweek - 1
+          if lweek == 0
+            lweek = 53
+          end
+
+          kpis[:speed_per_week][cweek]
+        end
+
+        def current_month_speed(kpis)
+          cmonth = DateTime.now.month
+
+          kpis[:speed_per_month][cmonth]
+        end
+        
+        def last_month_speed(kpis)
+
+        end
+
+        def current_week_projection(kpis)
+
+        end
+
+        def current_month_projection(kpis)
+
+        end
+
+        def last_month_projection(kpis)
+
+        end
+
+        def last_week_projection(kpis)
+
+        end
+
         def get_month gh_date
-          gh_date.split("-")[1]
+          gh_date.split("-")[1].to_i
+        end
+
+        def get_week gh_date
+          ## "2016-1-1" -> split "-" -> ["2016", "1", "1"] -> map to_i ->  
+          ## [2016, 1, 1], then "explode"
+          ## Maybe Mongo could do this also
+          dt = DateTime.new *gh_date.split("-").collect(:to_i)
+          return dt.cweek
         end
 
       end
